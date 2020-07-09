dd824fc19cba9a107fd8641b949ad69fd50ffe4c
Gargi Bansal <gban550@aucklanduni.ac.nz>
2020-07-09 16:03:26 +1200

    Fixed ID generation of shapes to be static per shape

--- Macros/Cuboid.py
+++ Macros/Cuboid.py
@@ -3,9 +3,13 @@ from Placement import Placement
 import FreeCAD
 
 class Cuboid(Shape):
-    def __init__(self, doc, id, dimension):
+    NEXT_ID = 1
+
+    def __init__(self, doc, dimension):
+        id = "Box" + str(Cuboid.NEXT_ID)
         super().__init__(id, dimension)
         doc.addObject("Part::Box", id)
         doc.getObject(id).Length = dimension[0]
         doc.getObject(id).Width = dimension[1]
         doc.getObject(id).Height = dimension[2]
+        Cuboid.NEXT_ID += 1
--- Macros/Cylinder.py
+++ Macros/Cylinder.py
@@ -3,9 +3,13 @@ from Placement import Placement
 import FreeCAD
 
 class Cylinder(Shape):
-    def __init__(self, doc, id, dimension):
+    NEXT_ID = 1
+
+    def __init__(self, doc, dimension):
+        id = "Cylinder" + str(Cylinder.NEXT_ID)
         super().__init__(id, dimension)
        	doc.addObject("Part::Cylinder", id)
         doc.getObject(id).Radius = dimension[0]
         doc.getObject(id).Height = dimension[1]
         doc.getObject(id).Angle = dimension[2]
+        Cylinder.NEXT_ID += 1
