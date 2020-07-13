bdb101ee2cf4242e813afafb88c1038ac15444e9
Gargi Bansal <gargi.bansal@gmail.com>
2020-07-13 17:30:07 +1200

    Partially fixed crashing error with drawing centrelines

--- Macros/HoleInBox.py
+++ Macros/HoleInBox.py
@@ -1,13 +1,26 @@
 from Shape import Shape
+from CentrelineInfo import CentrelineInfo
 import FreeCAD
 
 class HoleInBox(Shape):
     NEXT_ID = 1
-    ROTATIONS = [FreeCAD.Rotation(90, 0, 0), FreeCAD.Rotation(0, 90, 0), FreeCAD.Rotation(0, 0, 90)]
+    ROTATIONS = [
+        FreeCAD.Rotation(90, 0, 0), 
+        FreeCAD.Rotation(0, 90, 0), 
+        FreeCAD.Rotation(0, 0, 90)
+    ]
+
+    @staticmethod
+    def generateCentrelines(dimension):
+        return [
+            CentrelineInfo(dimension/2, dimension/2, None, 0, dimension, dimension/2 + 10),
+            CentrelineInfo(None, dimension/2, dimension/2, 0, dimension, dimension/2 + 10),
+            CentrelineInfo(dimension/2, None, dimension/2, 0, dimension, dimension/2 + 10),
+        ]
 
     def __init__(self, doc, dimension, matrixPos):
         id = "HoleInBox" + str(HoleInBox.NEXT_ID)
-        super().__init__(id, dimension, HoleInBox.ROTATIONS)
+        super().__init__(id, dimension, HoleInBox.ROTATIONS, HoleInBox.generateCentrelines(dimension))
         
         cubeID = "HoleInBoxCuboid" + str(HoleInBox.NEXT_ID)
         doc.addObject("Part::Box", cubeID)
--- Macros/Shape.py
+++ Macros/Shape.py
@@ -1,11 +1,11 @@
 import random
 
 class Shape:
-    def __init__(self, id, dimension, rotations, centrelineInfo = None):
+    def __init__(self, id, dimension, rotations, centrelines = None):
         self.id = id
         self.dimension = dimension
         self.rotations = rotations
-        self.CENTRELNIES = centrelineInfo
+        self.CENTRELNIES = centrelines
         self.centrelineInfo = None
 
     def getRandomRotation(self):
