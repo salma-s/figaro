67ca79ddd5aa4f9ca62f04811e44aca7108cd8e9
Gargi Bansal <gargi.bansal@gmail.com>
2020-08-08 14:41:59 +1200

    Added progress to SVG modification file

--- Macros/src/FormatLineStyling.py
+++ Macros/src/FormatLineStyling.py
@@ -7,11 +7,14 @@ TEMP_FILE_NAME = "temp.svg"
 
 ALL_SVGS = [];
 
+print("Finding SVGs to modify.")
 for root, dirs, files in os.walk(DIRECTORY):
     for file in files:
         if file.endswith("Orthographic.svg"):
              ALL_SVGS.append(os.path.join(root, file))
+print("Found " + str(len(ALL_SVGS)) + " to modify.")
 
+print("Making modifications...")
 for filename in ALL_SVGS:
   temp = open(DIRECTORY + TEMP_FILE_NAME, "w")
   doc = xml.dom.minidom.parse(filename)
@@ -20,14 +23,12 @@ for filename in ALL_SVGS:
     if 'stroke' in t.attributes.keys():
       if (t.attributes['stroke'].value=="#000000"):
         t.setAttribute('stroke-width', "20")
-        print("Changed solid line width")
       if 'stroke-dasharray' in t.attributes.keys():
         if (t.attributes['stroke'].value=="#ff0000"):
           t.setAttribute('stroke-dasharray', "60,30,20,30")
-          print("Changed centreline spacing")
         if (t.attributes['stroke'].value=="#afafaf"):
           t.setAttribute('stroke-dasharray', "60,60")
-          print("Changed hidden line spacing")
   temp.write(doc.toprettyxml())
   temp.close()
   os.rename(DIRECTORY + TEMP_FILE_NAME, filename)
+print("Modifications completed!")
